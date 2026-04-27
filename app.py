d0a4a786c87ebee0cb04f0a5232df839785b7cea
adekeyeyusuf520-commits <adekeyeyusuf520@gmail.com>
2026-04-27 20:39:37 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -41,4 +41,5 @@ def extract_text():
         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("port", 5000))
+    app.run(host="0.0.0.0", port=port)
